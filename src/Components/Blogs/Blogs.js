2e97cff61191ec4dadc693e8b890c04f3247ca4d
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-12 01:17:11 +0600

    Adding answerof question no 2

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 const Blogs = () => {
   return (
-    <div>
-      <h2 className='text-center text-2xl'>     
+    <div className='container'>
+      <h2 className='font-2xl font-bold'>     
       13.1 Difference between `javascript` and `nodejs`
-      <p> Answer : Javascirpt is a progamming language that is usually used to written  scripte in the clientside , can only be run in the browser, and also can play with dom and HTML and also use in front end development Where as Node is a java script runtime environment which is usually use in seversite and javascript can run outside in the browser with Node.</p>
+      <p className='text-center  font-semibold text-xl'> Answer : Javascirpt is a progamming language that is usually used to written  scripte in the clientside , can only be run in the browser, and also can play with dom and HTML and also use in front end development Where as Node is a java script runtime environment which is usually use in seversite and javascript can run outside in the browser with Node.</p>
       </h2>
-      <h2 className='text-center text-2xl'>
+      <h2 className='font-2xl font-bold'>
     13.2 When should you use `nodejs` and when should you use `mongodb`
+    
+    <p className='text-center  font-semibold text-xl'>
+    The  MongoDB is a database where we can store data and NodeJS helps us to to connect our front end site or client site  to database.
+    </p>
+
     </h2>
-    <h2 className='text-center text-2xl'> 
+    <h2 className='font-2xl font-bold'> 
     13.3 Differences between `sql` and `nosql` databases.
     </h2>
-    <h2 className='text-center text-2xl'> 
+    <h2 className='font-2xl font-bold'> 
     13.4 What is the purpose of `jwt` and how does it work
     </h2>
     </div>
