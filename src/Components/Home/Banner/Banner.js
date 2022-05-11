8b4b2ac39106677a58ba30891b236b3df96c6979
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-12 00:53:31 +0600

    Banner image added and check

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -6,21 +6,22 @@ const Banner = () => {
   <div className="carousel-inner relative w-full overflow-hidden">
     <div className="carousel-item active relative float-left w-full">
       <img
-        src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+        src="https://i.ibb.co/N6mzBw2/73802267-computer-shop-various-computer-parts-are-on-the-table-web-flat-vector-banner.webp"
         className="block w-full"
         alt="Wild Landscape"
       />
+      
     </div>
     <div className="carousel-item relative float-left w-full">
       <img
-        src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+        src="https://i.ibb.co/x8bTSbm/74106780-tienda-de-computadoras-varias-piezas-de-computadora-est-n-sobre-la-mesa-bandera-de-vector-p.webp"
         className="block w-full"
         alt="Camera"
       />
     </div>
     <div className="carousel-item relative float-left w-full">
       <img
-        src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+        src="https://i.ibb.co/7XxTn3w/100264904-inventory-manager-smart-nice-man-holding-a-laptop-and-counting-boxes-in-the-warehouse.webp"
         className="block w-full"
         alt="Exotic Fruits"
       />
