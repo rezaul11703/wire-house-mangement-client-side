5995e391964c34b4da7c3022564282b947c679ea
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-12 01:08:28 +0600

    Adding answer of question 1

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -5,6 +5,7 @@ const Blogs = () => {
     <div>
       <h2 className='text-center text-2xl'>     
       13.1 Difference between `javascript` and `nodejs`
+      <p> Answer : Javascirpt is a progamming language that is usually used to written  scripte in the clientside , can only be run in the browser, and also can play with dom and HTML and also use in front end development Where as Node is a java script runtime environment which is usually use in seversite and javascript can run outside in the browser with Node.</p>
       </h2>
       <h2 className='text-center text-2xl'>
     13.2 When should you use `nodejs` and when should you use `mongodb`
