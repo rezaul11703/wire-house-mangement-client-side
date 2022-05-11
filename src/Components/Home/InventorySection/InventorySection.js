e971a9ccf170724d40567f00b32e61f40b392586
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-12 01:22:06 +0600

    Adding button

--- src/Components/Home/InventorySection/InventorySection.js
+++ src/Components/Home/InventorySection/InventorySection.js
@@ -41,7 +41,10 @@ const InventorySection = () => {
 </div>)
       }
      
-
+     <div className='container  my-10 '>
+      <Link to={'/manageinventory'}> <button type="button" className="w-full inline-block px-6 py-2 border-2 border-blue-600 text-white-600 font-medium text-xs leading-normal uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"> Manage Inventory</button>
+      </Link>
+      </div>
 </div>
     </div>
 
